81b3f4ca8f96a1ef1135f647614596c639ac2a60
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-02-06 00:56:49 +0300

    addVideoLink

--- file.js
+++ file.js
@@ -1,5 +1,8 @@
 
 
+
+        
+
 const fromApi = (search)=>{
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
     fetch(url)
@@ -8,37 +11,58 @@ const fromApi = (search)=>{
 }
 
 const dataFunc = (dataMeal) =>{
-    console.log(dataMeal)
+    // console.log(dataMeal)
 
     const foodView = document.querySelector(".foodView")
     foodView.innerHTML = ``;
-    console.log(foodView)
-        for(const data of dataMeal){
+    // console.log(foodView)
+        for(const data of dataMeal){            
             const newDiv = document.createElement('div')
-                newDiv.innerHTML = `
-                <div class="col">
+            newDiv.innerHTML = `
+            <div class="col">
                     <div class="card">
-                        <img src="${data.strMealThumb}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">${data.strMeal}</h5>
-                            <p class="card-text">${data.strInstructions.slice(0,200)}</p>
-                        </div>
+                    <img src="${data.strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class=""> Name :  <span class="card-title fw-normal fs-5">${data.strMeal}</span> </h5>
+                    <h5 class="">Category :  <span class="card-title fw-normal fs-5">${data.strCategory}</span> </h5>
+                    <h5 class="">Area :  <span class="card-title fw-normal fs-5">${data.strArea}</span> </h5>
+
+                    <a href="${data.strYoutube}" class="px-3 py-2  text-center rounded-3 fs-5 fw-normal border border-danger text-decoration-none">
+                                                          Recipe
+                         <i class="fa-regular fa-circle-play mx-2" style="color: #f51414;"></i>
+                     </a>
+                    
                     </div>
-                </div>
-                `
-            foodView.appendChild(newDiv)    
+                    </div>
+                    </div>
+                    `
+                    foodView.appendChild(newDiv)    
+                    
+                    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++   
         }
+
 }
 
 const searchFood = () => {
     const searchField = document.getElementById("searchField")
     const searchValue =  searchField.value;
-    
     fromApi(searchValue)
     searchField.value = '';
     console.log(searchValue)
-
-
 }
 
 fromApi()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
